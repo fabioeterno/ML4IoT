3714831f9989632213513e86b39752325af069d7
Fabio Eterno <75701457+fabioeterno@users.noreply.github.com>
2022-01-31 09:01:55 +0100

    delivery version
    slow_service.py cleaned

--- HW3_ex2_Group3/notebook/slow_service.py
+++ HW3_ex2_Group3/notebook/slow_service.py
@@ -3,11 +3,7 @@ import cherrypy
 import json
 import os
 import base64
-#import RPi.GPIO as GPIO
 import time
-#from time import time
-#import adafruit_dht
-#import board
 import tensorflow as tf
 import tensorflow.lite as tflite
 from datetime import datetime
@@ -25,25 +21,20 @@ class Registry(object):
     def POST(self, *path, **query):
         try:
             
-            #read the bodyx
+            #read the body
             body = cherrypy.request.body.read()
             
             # # convert string into dictionary
-            body = json.loads(body)
-   
+            body = json.loads(body)  
             e = body.get('e')
             audio_str = e['v']
+        
             # DECODING THE TENSOR FROM STRING INTO BASE64 BYTES
             audio_b64bytes = audio_str.encode()
             audio = base64.b64decode(audio_b64bytes)
             
             audio = tf.io.parse_tensor(audio, out_type=tf.float32)
-            #print(type(audio))
-            #print(audio)
-
-
-            
-                
+           
             folder = os.path.join('{}.tflite'.format(model_name))
             interpreter = tflite.Interpreter(model_path=folder)
             interpreter.allocate_tensors()
@@ -54,14 +45,11 @@ class Registry(object):
             interpreter.invoke()
 
             predicted = interpreter.get_tensor(output_details[0]['index'])
-            #print(predicted[0])
             tmp = tf.math.softmax(predicted)*100
-            #print(tmp.numpy()[0])
             print([ np.round(x, 2) for x in tmp.numpy()[0] ])
             index = np.argmax(predicted[0])
             print("predicted: ", LABELS[index], index)
             
-
             label = {"l" : str(index)}
             
             return json.dumps(label)
